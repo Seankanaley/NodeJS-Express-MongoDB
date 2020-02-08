26cf0b11c390ac70d447a530abc4303106e93afa
Sean Kanaley <sean@postscriptlabs.com>
2020-02-08 01:15:42 -0800

    Node Callbacks and Error Handling

--- node-examples/app.js
+++ node-examples/app.js
@@ -3,12 +3,15 @@ const rect = require('./rectangle');
 function solveRect(l, w) {
     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
 
-    if (l <= 0 || w <= 0) {
-        console.log(`Rectangle dmensions must be greater than zero. Recieved: ${l}, ${w}`);
-    } else {
-        console.log(`Area of a rectangle: ${rect.area(l, w)}`);
-        console.log(`Perimeter of a rectangle: ${rect.perimeter(l, w)}`);
-    }
+    rect(l, w, (err, rectangle) => {
+        if (err) {
+            console.log('ERROR: ', err.message);
+        } else {
+            console.log(`Area of a rectangle with dimenstions ${l}, ${w} is: ${rectangle.area()}`);
+            console.log(`Perimeter of a rectangle with dimenstions ${l}, ${w} is: ${rectangle.perimeter()}`);
+        }
+    });
+    console.log('This statement is logged after the call  to rect()');
 }
 
 solveRect(2, 4);
--- node-examples/rectangle.js
+++ node-examples/rectangle.js
@@ -1,3 +1,20 @@
 // module.exports shorthand is exports
-exports.perimeter = (x, y) => 2 * (x + y);
-exports.area = (x, y) => (x * y);
+
+//We need to use module.exports instead of short hand because we're returning the entire function for the module instead of methods
+module.exports = (x, y, callback) => {
+    if (x <= 0 || y <= 0) {
+        callback(new Error(`Rectangle dmensions must be greater than zero. Recieved: ${x}, ${y}`));
+    } else {
+        setTimeout(() =>
+            callback(null, {
+                //Since X and Y are available in the outer function, we no longer need to pass them in to the arguments, this is due to closures
+                perimeter: () => 2 * (x + y),
+                area: () => (x * y)
+            }),
+            2000
+        );
+    }
+};
+
+
+
